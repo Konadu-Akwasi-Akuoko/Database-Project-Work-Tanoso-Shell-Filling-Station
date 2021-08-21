832900df29183aa7e58ceeb0f826665e2f705ea9
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2021-08-21 20:11:45 +0000

    Update the Labels when product is selected.

--- src/main/java/com/example/tanosofillingstation/EmployeeWindowController.java
+++ src/main/java/com/example/tanosofillingstation/EmployeeWindowController.java
@@ -1,10 +1,15 @@
 package com.example.tanosofillingstation;
 
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+
+import java.util.Objects;
 
 public class EmployeeWindowController {
     @FXML
@@ -25,13 +30,22 @@ public class EmployeeWindowController {
     private Label LBProductUpdate;
     @FXML
     private Label LBPricePerLitreUpdate;
+    @FXML
+    private TextField TFLitresBought;
+    @FXML
+    private TextField TFCostPrice;
+    @FXML
+    private TextField TFCustomerName;
+    @FXML
+    private TextField TFVehicleNumber;
+    @FXML
+    private TextField TFTelNumber;
 
     private enum CurrentButton {
         Petrol,
         Diesel,
         Fluid
     }
-
     CurrentButton currentButton;
 
     @FXML
@@ -39,9 +53,6 @@ public class EmployeeWindowController {
         //Change the enum current variable to petrol and pass it to the ChangeProduct function.
         currentButton = CurrentButton.Petrol;
         ChangeProduct(currentButton);
-        //When clicked, cast a button to use an action event, then use the getText to find the name of the string.
-        Button bt = (Button) event.getSource();
-        System.out.println(bt.getText());
     }
 
     @FXML
@@ -58,7 +69,7 @@ public class EmployeeWindowController {
         ChangeProduct(currentButton);
     }
 
-    //This function switches on and off the gridpanes, always the appropriate gridpane is shown.
+    //This function switches on and off the gridpanes using the buttons, always the appropriate gridpane is shown.
     private void ChangeProduct(CurrentButton currentButton) {
         switch (currentButton) {
             case Petrol:
@@ -78,4 +89,125 @@ public class EmployeeWindowController {
         }
     }
 
+    @FXML
+    private void PetrolProdButtonOnAction() {
+        LBTypeUpdate.setText("Gasoline");
+        LBProductUpdate.setText("Petrol");
+        LBPricePerLitreUpdate.setText("GH¢5.195");
+    }
+
+    @FXML
+    private void SuperPetrolProdButtonOnAction() {
+        LBTypeUpdate.setText("Gasoline");
+        LBProductUpdate.setText("Super Petrol");
+        LBPricePerLitreUpdate.setText("GH¢5.265");
+    }
+
+    @FXML
+    private void PetrolEngineOilProdButtonOnAction() {
+        LBTypeUpdate.setText("Gasoline");
+        LBProductUpdate.setText("Engine Oil");
+        LBPricePerLitreUpdate.setText("GH¢22.5");
+    }
+
+    @FXML
+    private void DieselProdButtonOnAction() {
+        LBTypeUpdate.setText("Diesel");
+        LBProductUpdate.setText("Diesel");
+        LBPricePerLitreUpdate.setText("GH¢5.095");
+    }
+
+    @FXML
+    private void DieselEngineOilProdButtonOnAction() {
+        LBTypeUpdate.setText("Diesel");
+        LBProductUpdate.setText("Engine Oil");
+        LBPricePerLitreUpdate.setText("GH¢22.5");
+    }
+
+    @FXML
+    private void ATFProdButtonOnAction() {
+        LBTypeUpdate.setText("Fluids");
+        LBProductUpdate.setText("Automatic Transmission Fluid D3");
+        LBPricePerLitreUpdate.setText("GH¢35");
+    }
+
+    @FXML
+    private void MotorFlushProdButtonOnAction() {
+        LBTypeUpdate.setText("Fluids");
+        LBProductUpdate.setText("Motor Flush");
+        LBPricePerLitreUpdate.setText("GH¢13");
+    }
+
+    @FXML
+    private void BrakeFluidProdButtonOnAction() {
+        LBTypeUpdate.setText("Fluids");
+        LBProductUpdate.setText("Brake Fluid Dot 3");
+        LBPricePerLitreUpdate.setText("GH¢23");
+    }
+
+    @FXML
+    private void FuelInjectorProdButtonOnAction() {
+        LBTypeUpdate.setText("Fluids");
+        LBProductUpdate.setText("Fuel Injector");
+        LBPricePerLitreUpdate.setText("GH¢15");
+    }
+
+    @FXML
+    private void LitresBoughtOnInputTextChanges() {
+        //Check if the Fuel type and product is of a certain type if so change the text in the cost price text box.
+        //SQL statements must be here.
+        double price = 0;
+        if (Objects.equals(LBTypeUpdate.getText(), "Gasoline") && Objects.equals(LBProductUpdate.getText(), "Petrol")) {
+            price = Math.round((Double.parseDouble(TFLitresBought.getText()) * 5.195)*100.0)/100.0;
+            TFCostPrice.setText("GH¢"+price);
+        }
+        else if(Objects.equals(LBTypeUpdate.getText(), "Gasoline") && Objects.equals(LBProductUpdate.getText(), "Super Petrol")){
+            price = Math.round((Double.parseDouble(TFLitresBought.getText()) * 5.265)*100.0)/100.0;
+            TFCostPrice.setText("GH¢"+price);
+        }
+        else if(Objects.equals(LBTypeUpdate.getText(), "Gasoline") && Objects.equals(LBProductUpdate.getText(), "Engine Oil")){
+            price = Math.round((Double.parseDouble(TFLitresBought.getText())* 22.5)*100.0)/100.0;
+            TFCostPrice.setText("GH¢"+price);
+        }
+        else if(Objects.equals(LBTypeUpdate.getText(), "Diesel") && Objects.equals(LBProductUpdate.getText(), "Diesel")){
+            price = Math.round((Double.parseDouble(TFLitresBought.getText())* 5.095)*100.0)/100.0;
+            TFCostPrice.setText("GH¢"+price);
+        }
+        else if(Objects.equals(LBTypeUpdate.getText(), "Diesel") && Objects.equals(LBProductUpdate.getText(), "Engine Oil")){
+            price = Math.round((Double.parseDouble(TFLitresBought.getText())* 22.5)*100.0)/100.0;
+            TFCostPrice.setText("GH¢"+price);
+        }
+        else if(Objects.equals(LBTypeUpdate.getText(), "Fluids") && Objects.equals(LBProductUpdate.getText(), "Automatic Transmission Fluid D3")){
+            price = Math.round((Double.parseDouble(TFLitresBought.getText())* 35)*100.0)/100.0;
+            TFCostPrice.setText("GH¢"+price);
+        }
+        else if(Objects.equals(LBTypeUpdate.getText(), "Fluids") && Objects.equals(LBProductUpdate.getText(), "Brake Fluid Dot 3")){
+            price = Math.round((Double.parseDouble(TFLitresBought.getText())* 23)*100.0)/100.0;
+            TFCostPrice.setText("GH¢"+price);
+        }
+        else if(Objects.equals(LBTypeUpdate.getText(), "Fluids") && Objects.equals(LBProductUpdate.getText(), "Fuel Injector")){
+            price = Math.round((Double.parseDouble(TFLitresBought.getText())* 15)*100.0)/100.0;
+            TFCostPrice.setText("GH¢"+price);
+        }
+        else if(Objects.equals(LBTypeUpdate.getText(), "Fluids") && Objects.equals(LBProductUpdate.getText(), "Motor Flush")){
+            price = Math.round((Double.parseDouble(TFLitresBought.getText())* 13)*100.0)/100.0;
+            TFCostPrice.setText("GH¢"+price);
+        }
+    }
+
+    @FXML
+    private void CancelButtonOnAction(ActionEvent event){
+        //When the cancel button is pressed all text fields needs to be cleared.
+        TFCustomerName.setText("");
+        TFCustomerName.setPromptText("Name");
+        TFVehicleNumber.setText("");
+        TFVehicleNumber.setPromptText("Vehicle number");
+        TFTelNumber.setText("");
+        TFTelNumber.setPromptText("Telephone number");
+
+        TFLitresBought.setText("");
+        TFLitresBought.setPromptText("Litres");
+        TFCostPrice.setText("");
+        TFCostPrice.setPromptText("Price");
+    }
 }
